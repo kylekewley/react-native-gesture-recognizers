ed113f6bf2edcf5e400daad186e12ee13c6e9f23
Kyle Kewley <kylek61294@gmail.com>
2017-02-08 18:13:45 -0800

    Removed unused classes

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 'use strict';
-import pannable from './recognizers/pannable';
-import GestureRecognizer from './recognizers/GestureRecognizer';
 import DoubleTouchableHighlight from './recognizers/DoubleTouchableHighlight';
 
-module.exports = { pannable, GestureRecognizer, DoubleTouchableHighlight }
+module.exports = { DoubleTouchableHighlight }
